009ad9ccdfa46451654dcefce196cad28ce3254f
InasseY <inasse.youssouf@gmail.com>
2021-02-17 15:00:42 +0100

    boucle while with variable

---
+++ Partie3_Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Partie3_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            int n = 0;
+
+            while (n < 10)
+            {
+                Console.WriteLine($"Bonjour, je suis le message n°{n}");
+                n++;
+            }
+        }
+    }
+}
